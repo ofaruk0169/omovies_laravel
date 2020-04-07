64ffa4343dc8876e25bad2e27deb79e480193613
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-07 18:28:12 +0100

    added some css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Currently on 4:21</title>
+    <title>Weather App</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="/js/app.js" defer></script>
 </head>
-<body>
-    
+<body class="bg-blue-500">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, impedit nam consequuntur provident eos nostrum debitis ipsum dolorem beatae cupiditate est officia sit, animi nemo quidem minima, culpa enim dolor?
 </body>
 </html>
