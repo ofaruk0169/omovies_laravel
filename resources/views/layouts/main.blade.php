55da32114ed7da4d37712ed8b3ca1298a7244c0a
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-14 12:04:58 +0100

    livewire functionality has been added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movie App</title>
+    <title>OMovies</title>
 
     <link rel="stylesheet" href="/css/main.css">
+    <livewire:styles>
 </head>
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
@@ -29,14 +30,7 @@
             
             </ul> 
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text" class="bg-gray-800 text-sm rounded-full w-65 px-4 pl-8 py-1
-                    focus:outline-none focus:shadow-outline" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 31.25"><path d="M22.44 19.732l-4.586-4.586a.5.5 0 00-.707 0l-.647.647-1.362-1.363a7.526 7.526 0 10-.708.708l1.363 1.362-.646.646a.5.5 0 000 .707l4.585 4.586a1.914 1.914 0 002.708-2.707zM3 9.5A6.5 6.5 0 119.5 16 6.508 6.508 0 013 9.5zm18.732 12.232a.915.915 0 01-1.293 0L16.207 17.5l1.293-1.293 4.232 4.233a.915.915 0 010 1.292z" data-name="Search"/><text y="40" font-size="5" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">Created by Kmg Design</text><text y="45" font-size="5" font-weight="bold"
-                            font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">from the Noun Project</text></svg>
-                    </div>
-                </div>
+               <livewire:search-dropdown>
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
@@ -47,5 +41,6 @@
         </div>
     </nav>
     @yield('content')
+    <livewire:scripts>
 </body>
 </html>
---
+++ resources/views/livewire/search-dropdown.blade.php
@@ -0,0 +1,34 @@
+<div class="relative mt-3 md:mt-0">
+    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-65 px-4 pl-8 py-1
+    focus:outline-none focus:shadow-outline" placeholder="Search">
+    <div class="absolute top-0">
+        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 31.25"><path d="M22.44 19.732l-4.586-4.586a.5.5 0 00-.707 0l-.647.647-1.362-1.363a7.526 7.526 0 10-.708.708l1.363 1.362-.646.646a.5.5 0 000 .707l4.585 4.586a1.914 1.914 0 002.708-2.707zM3 9.5A6.5 6.5 0 119.5 16 6.508 6.508 0 013 9.5zm18.732 12.232a.915.915 0 01-1.293 0L16.207 17.5l1.293-1.293 4.232 4.233a.915.915 0 010 1.292z" data-name="Search"/><text y="40" font-size="5" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">Created by Kmg Design</text><text y="45" font-size="5" font-weight="bold"
+            font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">from the Noun Project</text></svg>
+    </div>
+    @if (strlen($search) > 2)
+        <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+            @if ($searchResults->count() > 0)
+            <ul>
+                @foreach ($searchResults as $result)
+                    <li class="border-b border-gray-700">
+                        <a href="{{route('movies.show', $result['id'])}}" class="block 
+                        hover:bg-gray-700 px-3 py-3 flex items-center">
+                        @if ($result['poster_path'])
+                            <img src="https://image.tmdb.org/t/p/w92/{{$result['poster_path']}}" alt="poster"
+                            class="w-8">
+                        @else
+                            <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                        @endif
+                        
+                        <span class="ml-4"> {{$result['title']}} </span>
+                        </a>
+                    </li>
+                @endforeach   
+            </ul>
+            @else
+                <div class="px-3 py-3">No results for  "{{$search}}"</div>
+            @endif
+        </div>
+    @endif
+</div>
+
