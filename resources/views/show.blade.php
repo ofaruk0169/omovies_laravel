83299f75fd0de71ba17337e0194886040f284783
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-14 17:15:42 +0100

    modal for play button has been added

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -51,6 +51,31 @@
                     </div>
                 @endif
 
+                <div 
+                    style="background-color: rgba(0, 0, 0, .5);"
+                    class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                    >
+                        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                            <div class="bg-gray-900 rounded">
+                                <div class="flex justify-end pr-4 pt-2">
+                                    <button class="text-3xl leading-none hover:text-gray-300">&times;</button
+                                    >
+
+                                </div>
+                                <div class="moal-body px-8 py-8">
+                                    <div class="responsive-container overflow-hidden relative" style="
+                                    padding-top:56.25%">
+                                    <iframe width="560" height="315" class="responsive-iframe absolute
+                                    top-0 left-0 w-full h-full" src="https://youtube.com/embed/
+                                    D9oIu6jiYLk" style="border:0;" allow="autoplay; encrypted-media"
+                                    allowfullscreen></iframe>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                </div>
 
             </div>
         </div>
