c5221265179950751591a73b58429e2c62bf5cf7
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-04-15 09:09:11 +0100

    modal for individual images has been added

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,6 +1,13 @@
 <div class="relative mt-3 md:mt-0" x-data="{ isOpen: true}" @click.away="isOpen = false">
     <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-65 px-4 pl-8 py-1
     focus:outline-none focus:shadow-outline" placeholder="Search"
+    x-ref="search"
+    @keydown.window="
+        if(event.keyCode == 191) {
+            event.preventDefault();
+            $refs.search.focus();
+        }
+    "
     @focus="isOpen = true"
     @keydown="isOpen = true"
     @keydown.escape.window="isOpen = false"
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -118,14 +118,21 @@
 
     {{-- end of actor info  --}}
 
-    <div class="movie-images border-b border-gray-800">
+    <div class="movie-images" x-data="{ isOpen: false, image: ''}">
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Images</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
                 @foreach ($movie['images']['backdrops'] as $image)
                     @if ($loop->index < 9)
                         <div class="mt-8">
-                            <a href="#">
+                            <a 
+                                @click.prevent="
+                                    isOpen = true
+                                    image='{{'https://image.tmdb.org/t/p/original/'.$image['file_path']}}'    
+                                "
+                                href="#"
+                                
+                            >
                                 <img src={{'https://image.tmdb.org/t/p/w500/'.$image['file_path']}} alt="image1" 
                                 class="hover:opacity-75 transition ease-in-out duration-150">
                             </a>
@@ -133,6 +140,31 @@
                     @endif 
                 @endforeach                               
                 </div>
+
+
+{{-- modal for the images  --}}
+
+                    <div
+                        style="background-color: rgba(0, 0, 0, .5);"
+                        class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                        x-show="isOpen"
+                    >
+                        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                            <div class="bg-gray-900 rounded">
+                                <div class="flex justify-end pr-4 pt-2">
+                                    <button 
+                                    @click.="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="text-3xl leadning-none hover:text-gray-300">&times;</button>
+                                </div>
+                                <div class="modal-body px-8 py-8">
+                                   <img :src="image" alt="poster">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
 
 
 
